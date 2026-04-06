c4af3fc83fb7afe63be1c98260dfe016cd5a52ce
Rina Ratiyapinan <rina.ratiyapinan@gmail.com>
2026-04-06 13:05:03 +0300

    Updates

---
+++ static/js/488.2f217433.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(globalThis.webpackChunkproduct_tool_app=globalThis.webpackChunkproduct_tool_app||[]).push([[488],{488(p,a,o){new WeakMap;new Set}}]);
+//# sourceMappingURL=488.2f217433.chunk.js.map
--- static/js/488.663ea6d5.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkproduct_tool_app=self.webpackChunkproduct_tool_app||[]).push([[488],{488:(p,e,t)=>{new WeakMap;new Set}}]);
-//# sourceMappingURL=488.663ea6d5.chunk.js.map
